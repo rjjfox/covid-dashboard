2e2125a42e46baa831d2c0c50a903439b37491d7
rjjfox <ryanfox212@gmail.com>
2021-01-08 23:12:03 +0000

    Add FR plots and numbers

--- coviddashboard/routes.py
+++ coviddashboard/routes.py
@@ -1,8 +1,9 @@
 from coviddashboard import app
 import json, plotly
 from flask import render_template
-from coviddashboard.analysis.uk_analysis import (
-    pull_data,
+from coviddashboard.analysis.country_analysis import (
+    get_uk_data,
+    get_france_data,
     return_figures,
     get_current_numbers,
     compare_against_n_days_ago,
@@ -13,11 +14,27 @@ from coviddashboard.analysis.uk_analysis import (
 @app.route("/index")
 def index():
 
-    df = pull_data()
-    figures = return_figures(df)
-    current_cases, current_deaths = get_current_numbers(df)
-    previous_week_cases, previous_week_deaths = compare_against_n_days_ago(7, df)
-    previous_month_cases, previous_month_deaths = compare_against_n_days_ago(30, df)
+    uk_df = get_uk_data()
+    fr_df = get_france_data()
+
+    figures = return_figures(uk_df)
+    figures = figures + return_figures(fr_df)
+
+    uk_current_cases, uk_current_deaths = get_current_numbers(uk_df)
+    uk_previous_week_cases, uk_previous_week_deaths = compare_against_n_days_ago(
+        7, uk_df
+    )
+    uk_previous_month_cases, uk_previous_month_deaths = compare_against_n_days_ago(
+        30, uk_df
+    )
+
+    fr_current_cases, fr_current_deaths = get_current_numbers(fr_df)
+    fr_previous_week_cases, fr_previous_week_deaths = compare_against_n_days_ago(
+        7, fr_df
+    )
+    fr_previous_month_cases, fr_previous_month_deaths = compare_against_n_days_ago(
+        30, fr_df
+    )
 
     # plot ids for the html id tag
     ids = [f"figure-{i}" for i, _ in enumerate(figures)]
@@ -29,10 +46,16 @@ def index():
         "index.html",
         ids=ids,
         figuresJSON=figuresJSON,
-        current_cases=f"{current_cases:,.0f}",
-        current_deaths=f"{current_deaths:,.0f}",
-        previous_week_cases=f"{previous_week_cases:.2%}",
-        previous_week_deaths=f"{previous_week_deaths:.2%}",
-        previous_month_cases=f"{previous_month_cases:.2%}",
-        previous_month_deaths=f"{previous_month_deaths:.2%}",
+        uk_current_cases=f"{uk_current_cases:,.0f}",
+        uk_current_deaths=f"{uk_current_deaths:,.0f}",
+        uk_previous_week_cases=f"{uk_previous_week_cases:.0%}",
+        uk_previous_week_deaths=f"{uk_previous_week_deaths:.0%}",
+        uk_previous_month_cases=f"{uk_previous_month_cases:.0%}",
+        uk_previous_month_deaths=f"{uk_previous_month_deaths:.0%}",
+        fr_current_cases=f"{fr_current_cases:,.0f}",
+        fr_current_deaths=f"{fr_current_deaths:,.0f}",
+        fr_previous_week_cases=f"{fr_previous_week_cases:.0%}",
+        fr_previous_week_deaths=f"{fr_previous_week_deaths:.0%}",
+        fr_previous_month_cases=f"{fr_previous_month_cases:.0%}",
+        fr_previous_month_deaths=f"{fr_previous_month_deaths:.0%}",
     )
